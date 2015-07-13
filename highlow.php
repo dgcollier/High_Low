a36b35ad4f29fb1c57cf5038d9d9be9f53b16767
dgcollier <david@gcollier.me>
2015-07-13 13:27:01 -0500

    Changed conditional to account for incorrect inputs other than strings (floats).

--- highlow.php
+++ highlow.php
@@ -3,11 +3,16 @@
 $rand = rand(1, 100);
     // echo $rand . PHP_EOL;
 
-fwrite(STDOUT, 'Guess a number between 1 and 100:');
+fwrite(STDOUT, 'Guess an integer between 1 and 100:');
 
 $userGuess = fgets(STDIN);
 
 // echo "You guessed {$userGuess}\n";
+
+if ($userGuess < 1 || $userGuess > 100 || !is_int($userGuess)) {
+    fwrite(STDOUT, "No, I said an integer between 1 and 100\n");
+    $userGuess = fgets(STDIN);
+}
 
 
 ?>
